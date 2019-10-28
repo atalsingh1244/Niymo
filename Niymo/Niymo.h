341958e7ad0e1479fdad5e016fe2087b1e810279
atalsingh1244 <atalsinghmgst@gmail.com>
2019-10-28 13:27:26 +0530

    Initial commit

---
+++ Niymo/Niymo.h
@@ -0,0 +1,19 @@
+//
+//  Niymo.h
+//  Niymo
+//
+//  Created by Atal Singh on 28/10/19.
+//  Copyright © 2019 Atal Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Niymo.
+FOUNDATION_EXPORT double NiymoVersionNumber;
+
+//! Project version string for Niymo.
+FOUNDATION_EXPORT const unsigned char NiymoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Niymo/PublicHeader.h>
+
+
---
+++ NiymoExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NiymoExamples
+//
+//  Created by Atal Singh on 28/10/19.
+//  Copyright © 2019 Atal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
