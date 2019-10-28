0e1363f9e839de3a86390b9ccce9106170145f6f
atalsingh1244 <atalsinghmgst@gmail.com>
2019-10-28 13:50:59 +0530

    Added test cases

---
+++ Niymo/Niymo.swift
@@ -0,0 +1,23 @@
+//
+//  Niymo.swift
+//  Niymo
+//
+//  Created by Atal Singh on 28/10/19.
+//  Copyright © 2019 Atal Singh. All rights reserved.
+//
+
+import Foundation
+
+public final class Niymo {
+    
+    let name = "Niymo"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- NiymoTests/NiymoTests.swift
+++ NiymoTests/NiymoTests.swift
@@ -10,25 +10,17 @@ import XCTest
 @testable import Niymo
 
 class NiymoTests: XCTestCase {
+    
+    var niymo: Niymo!
+    
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        niymo = Niymo()
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(niymo.add(a: 1, b: 2), 3)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
